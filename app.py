d66b8d527a5390108932fdce0171d2d46cc4b944
YunCheng <hh1253567@gmail.com>
2020-11-13 21:44:30 +0800

    ok

--- app.py
+++ app.py
@@ -108,21 +108,21 @@ def handle_message(event):
     elif '吸菸' in msg:
         message = TextSendMessage(text="吸菸傷身又不環保"+"\n"+"不如來吃蛋捲吧！好吃😋又便宜！"+"\n"+"心動不如馬上行動！😝😝")
         line_bot_api.reply_message(event.reply_token, message)  
-     elif '掰' in msg:
-        message = TextSendMessage(text="掰掰👋記得有空回來找我喔～～")
-        line_bot_api.reply_message(event.reply_token, message)  
-    elif '8' in msg:
-        message = TextSendMessage(text="掰掰👋記得有空回來找我喔～～")
-        line_bot_api.reply_message(event.reply_token, message)  
-    elif 'bye' in msg:
-        message = TextSendMessage(text="掰掰👋記得有空回來找我喔～～")
-        line_bot_api.reply_message(event.reply_token, message) 
-    elif '先走' in msg:
-        message = TextSendMessage(text="掰掰👋記得有空回來找我喔～～")
-        line_bot_api.reply_message(event.reply_token, message) 
-    elif '再見' in msg:
-        message = TextSendMessage(text="掰掰👋記得有空回來找我喔～～")
-        line_bot_api.reply_message(event.reply_token, message)       
+   #  elif '掰' in msg:
+    #    message = TextSendMessage(text="掰掰👋記得有空回來找我喔～～")
+     #   line_bot_api.reply_message(event.reply_token, message)  
+    #elif '8' in msg:
+     #   message = TextSendMessage(text="掰掰👋記得有空回來找我喔～～")
+      #  line_bot_api.reply_message(event.reply_token, message)  
+    #elif 'bye' in msg:
+     #   message = TextSendMessage(text="掰掰👋記得有空回來找我喔～～")
+      #  line_bot_api.reply_message(event.reply_token, message) 
+    #elif '先走' in msg:
+     #   message = TextSendMessage(text="掰掰👋記得有空回來找我喔～～")
+      #  line_bot_api.reply_message(event.reply_token, message) 
+    #elif '再見' in msg:
+     #   message = TextSendMessage(text="掰掰👋記得有空回來找我喔～～")
+      #  line_bot_api.reply_message(event.reply_token, message)       
     #elif '合作' in msg:
        # message = imagemap_message()
        # line_bot_api.reply_message(event.reply_token, message)
