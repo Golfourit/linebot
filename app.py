d4d20d34f01503fc036530de57160aa2c9ab47a2
YunCheng <hh1253567@gmail.com>
2020-11-13 21:22:09 +0800

    ok

--- app.py
+++ app.py
@@ -94,34 +94,34 @@ def handle_message(event):
         message = TextSendMessage(text="說到這個～～"+"\n"+"ㄟ果肉蛋捲✖️The Naivest 😉\n"+"蛋捲我本人要給你看看我們設計的海報啦！！有包刮價錢、口味、訂購單QRcode!快來看看吧"+"\n"+"可以到我們的訂購單看看～"+"\n"+"🔗https://docs.google.com/forms/d/e/1FAIpQLScdbpfT8a5uaLut0o7O2kO_N0m9xnrv7O49gVBBdRwMNk16SA/viewform"+"\n"+"還有IG.FB呦！"+"\n"+"🔗IG:https://instagram.com/handmade_eggroll?igshid=64g5czfwa23l"+"\n"+"🔗FB:https://m.facebook.com/ㄟ果肉-The-naivest-手工蛋捲-11415379361690")
         line_bot_api.reply_message(event.reply_token, message)   
     elif '車站' in msg:
-        message = TextSendMessage(text="(aww)我們ㄟ果肉商店最近的車站就是中壢火車站呦！"+"\n"+"俗話說麻雀雖小，五臟俱全。"+"\n"+"中壢車站常常有人說他很小很醜～但其實他確實蠻重要的一個車站呢！長知識了💪🧠🧠"+"\n"+"\n"+"Google地圖🔗👇👇"+"\n"+"中壢車站"+"\n"+"https://goo.gl/maps/rB5nPDYyKyKNb6q4A")
+        message = TextSendMessage(text="我們ㄟ果肉商店最近的車站就是中壢火車站呦！"+"\n"+"俗話說麻雀雖小，五臟俱全。"+"\n"+"中壢車站常常有人說他很小很醜～但其實他確實蠻重要的一個車站呢！長知識了💪🧠🧠"+"\n"+"\n"+"Google地圖🔗👇👇"+"\n"+"中壢車站"+"\n"+"https://goo.gl/maps/rB5nPDYyKyKNb6q4A")
         line_bot_api.reply_message(event.reply_token, message)    
     elif '網站' in msg:
-        message = TextSendMessage(text="(我們官網的連結在這！有興趣的都歡迎上來看看喔～～👏"+"\n"+"🔗🔗https://handmade-eggroll-3.jimdosite.com/")
+        message = TextSendMessage(text="我們官網的連結在這！有興趣的都歡迎上來看看喔～～👏"+"\n"+"🔗🔗https://handmade-eggroll-3.jimdosite.com/")
         line_bot_api.reply_message(event.reply_token, message)     
     elif '官網' in msg:
-        message = TextSendMessage(text="(我們官網的連結在這！有興趣的都歡迎上來看看喔～～👏"+"\n"+"🔗🔗https://handmade-eggroll-3.jimdosite.com/")
+        message = TextSendMessage(text="我們官網的連結在這！有興趣的都歡迎上來看看喔～～👏"+"\n"+"🔗🔗https://handmade-eggroll-3.jimdosite.com/")
         line_bot_api.reply_message(event.reply_token, message) 
-    elif '官方網網' in msg:
-        message = TextSendMessage(text="(我們官網的連結在這！有興趣的都歡迎上來看看喔～～👏"+"\n"+"🔗🔗https://handmade-eggroll-3.jimdosite.com/")
+    elif '官方網站' in msg:
+        message = TextSendMessage(text="我們官網的連結在這！有興趣的都歡迎上來看看喔～～👏"+"\n"+"🔗🔗https://handmade-eggroll-3.jimdosite.com/")
         line_bot_api.reply_message(event.reply_token, message) 
     elif '吸菸' in msg:
-        message = TextSendMessage(text="(，吸菸傷身又不環保"+"\n"+"不如來吃蛋捲吧！好吃😋又便宜！"+"\n"+"心動不如馬上行動！😝😝")
+        message = TextSendMessage(text="吸菸傷身又不環保"+"\n"+"不如來吃蛋捲吧！好吃😋又便宜！"+"\n"+"心動不如馬上行動！😝😝")
         line_bot_api.reply_message(event.reply_token, message)  
      elif '掰' in msg:
-        message = TextSendMessage(text="(掰掰👋記得有空回來找我喔～～")
+        message = TextSendMessage(text="掰掰👋記得有空回來找我喔～～")
         line_bot_api.reply_message(event.reply_token, message)  
     elif '8' in msg:
-        message = TextSendMessage(text="(掰掰👋記得有空回來找我喔～～")
+        message = TextSendMessage(text="掰掰👋記得有空回來找我喔～～")
         line_bot_api.reply_message(event.reply_token, message)  
     elif 'bye' in msg:
-        message = TextSendMessage(text="(掰掰👋記得有空回來找我喔～～")
+        message = TextSendMessage(text="掰掰👋記得有空回來找我喔～～")
         line_bot_api.reply_message(event.reply_token, message) 
     elif '先走' in msg:
-        message = TextSendMessage(text="(掰掰👋記得有空回來找我喔～～")
+        message = TextSendMessage(text="掰掰👋記得有空回來找我喔～～")
         line_bot_api.reply_message(event.reply_token, message) 
     elif '再見' in msg:
-        message = TextSendMessage(text="(掰掰👋記得有空回來找我喔～～")
+        message = TextSendMessage(text="掰掰👋記得有空回來找我喔～～")
         line_bot_api.reply_message(event.reply_token, message)       
     #elif '合作' in msg:
        # message = imagemap_message()
