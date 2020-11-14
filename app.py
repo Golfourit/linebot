39a9d035cc2a38329626e79d3c8c0513fb5dd3cf
YunCheng <hh1253567@gmail.com>
2020-11-14 10:23:00 +0800

    ok

--- app.py
+++ app.py
@@ -45,10 +45,14 @@ def callback():
 
 
 # 處理訊息
-@handler.add(MessageEvent, message=TextMessage,message1=TextMessage)
+@handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     msg = event.message.text 
-    
+    message = ImageSendMessage(
+    original_content_url='https://example.com/original.jpg',
+    preview_image_url='https://example.com/preview.jpg'
+    )  
+    line_bot_api.reply_message(event.reply_token, message)
     #massage1 =TextMessage #
      #if ('方舟' or '方舟啟智教養院' or '教養院') in msg:
        #   message =ImageSendMessage(
